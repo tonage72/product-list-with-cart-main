63c5bbd300578f55fc3dee3f5ae8615102d3fa38
David Fleming <tonage72@gmail.com>
2025-03-29 22:45:31 -0400

    Experimenting with function and fetch

--- javascript/script.js
+++ javascript/script.js
@@ -3,29 +3,23 @@ const cart = document.querySelector('.cart')
 
 cart.style.display = 'none'
 
-function getItemData() {
-	fetch('./data.json')
-		.then((data) => data.json())
-		.then((datajson) => addCards(datajson))
-}
-
-function addCards(datajson) {
-	datajson.forEach((element, index) => {
-		main.innerHTML += `
-			<div class="card">
-					<img src="${element.image.mobile}" alt="">
-					<button class="btn-add-to-cart">Add to Cart</button>
-					<h2>${element.category}</h2>
-					<h2>${element.name}</h2>
-					<p>${element.price}</p>
-					<h1>${index}</h1>
-			</div>
-		`
+fetch('./data.json')
+	.then((data) => data.json())
+	.then((datajson) => {
+		datajson.forEach((menuItem, index) => {
+			main.innerHTML += `
+				<div class="card">
+						<img src="${menuItem.image.mobile}" alt="">
+						<button class="btn-add-to-cart">Add to Cart</button>
+						<h2>${menuItem.category}</h2>
+						<h2>${menuItem.name}</h2>
+						<p>${menuItem.price}</p>
+						<h1>${index}</h1>
+				</div>
+			`
+		})
+		const addToCartBtns = document.querySelectorAll('.btn-add-to-cart')
+		addToCartBtns.forEach((btn, index) => {
+			btn.style.backgroundColor = 'red'
 	})
-	const addToCartBtns = document.querySelectorAll('.btn-add-to-cart')
-	addToCartBtns.forEach((btn, index) => {
-		btn.style.backgroundColor = 'red'
-	})
-}
-
-getItemData()
+})
